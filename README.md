343c95bc1adb8bb4c55d5e8b8c42db5b86d75af1
Gauraviitkgp <gauravs123456789@gmail.com>
2018-05-23 21:14:27 +0100

    Final version

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 # image_to_world_frame
+## Using 1to1 correspondence
 Converts the cordinates in image to world frame
 
-To do so, first select and image who has a all points at a single plane with known Z values. Open the pixel_to_world.cpp file and do the following changes
+To do so, first select and image who has a all points at a single plane with known Z values. Open the using1to1.cpp file and do the following changes
 
-    Change the camera matrix (line number 90)
+    Change the camera matrix (line number 56)
     Uncomment the distortion coefficients if any
     Draw the 1to1 correspondence of a point in image to its known world cordinates and edit the imagepointpix and objectpointsm in the same 1to1 order
   
-After the prelimnary changes for debug mode change the #if statement(line 130) to 
+After the prelimnary changes for debug mode change the #if statement(line 67) to 
   
     #if 0
     This would change the code to debug mode in which you can check if the code is giving the correct output. Ensure that MIN is less than 100. MIN is the minimum error in pixels. 
@@ -15,11 +16,31 @@ For testing mode change it to
     
     #if 1
     This would change the code to testing mode wherein you can check whether the desired output is correct or not. Each second it will try to take an input and will output Point matrix. If you dont provide input it will take previous inpurs
+    Check whether camera_cordinates align with the output you got
  
- ## Run time arguments
+ ### Run time arguments
  
     argv[1] Source image
     argv[2] Max Iterations 
 
-Change the maximum iterations if the error is greater than 100
-    
+Change the maximum iterations if the error is greater than 100.
+
+## Using camera cordinates
+This file is a general file, if given camera cordinates and (translation matrix or rotation matrix) we can calculate the world cordinates for any given pixel cordinates
+
+Use the formula 
+
+		Camera_cordinates=-rotationMatrix.t()*tvec
+   
+Any two of this is known and then world cordinates of every pixel of the image could be known
+
+###To do
+		Change the Camera matrix(line 37)
+		Enter either of two of (camera cordinates rotation matrix or tvec)
+		modify the code in accordance with above formula
+
+And TADAAA! check your results. 
+
+## Serious notes
+Flat earthies please stay away, earh is not in a single plane the code may not work for you
+
--- src/using1to1.cpp
+++ src/using1to1.cpp
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <vector>
 #include <iostream>
@@ -6,7 +5,6 @@
 #include <opencv2/highgui/highgui.hpp>
 #include "opencv2/calib3d/calib3d.hpp"
 #include <opencv2/imgproc/imgproc.hpp>
-#include <fstream>
 #include <opencv2/core/core.hpp>
 
 using namespace cv;
@@ -30,54 +28,19 @@ int main(int argc, char const *argv[])
 	vector<Point2f> imagePointsPix;
 	vector<Point3f> objectPointsm;
 
-//	imagePointsPix.push_back(Point2f(0.,284.));
-	//imagePointsPix.push_back(Point2f(146.,283.));
-	//imagePointsPix.push_back(Point2f(307.,283.));
-	//imagePointsPix.push_back(Point2f(360.,282.));
-//	imagePointsPix.push_back(Point2f(624.,281.));
-//	imagePointsPix.push_back(Point2f(40.,173.));	
-	 imagePointsPix.push_back(Point2f(105.,161.));
+	imagePointsPix.push_back(Point2f(105.,161.));
 	imagePointsPix.push_back(Point2f(299.,164.));
 	imagePointsPix.push_back(Point2f(510.,164.));
-	//imagePointsPix.push_back(Point2f(619.,276.));
-	// imagePointsPix.push_back(Point2f(28.,176.));
-	 imagePointsPix.push_back(Point2f(150.,52.));
+	imagePointsPix.push_back(Point2f(150.,52.));
 	imagePointsPix.push_back(Point2f(299.,52.));
 	imagePointsPix.push_back(Point2f(472.,52.));
-	//imagePointsPix.push_back(Point2f(566.,171.));
-//	imagePointsPix.push_back(Point2f(636.,96.));
-//	imagePointsPix.push_back(Point2f(12.,36.));
-	// imagePointsPix.push_back(Point2f(187.,96.));
-	// imagePointsPix.push_back(Point2f(187.,91.));
-	// imagePointsPix.push_back(Point2f(307.,89.));
-	// imagePointsPix.push_back(Point2f(408.,96.));
-	// imagePointsPix.push_back(Point2f(529.,89.));
-	// imagePointsPix.push_back(Point2f(636.,86.));
 
-
-	//objectPointsm.push_back(Point3f(1., 2., 0.));
-	//objectPointsm.push_back(Point3f(1., 1., 0.));
-	//objectPointsm.push_back(Point3f(1.,-0., 0.));
-	//objectPointsm.push_back(Point3f(1., -1., 0.));
-	//objectPointsm.push_back(Point3f(1., -2., 0.));
-	//objectPointsm.push_back(Point3f(2., 2., 0.));
 	objectPointsm.push_back(Point3f(2., 4., 0.));
 	objectPointsm.push_back(Point3f(2.,3., 0.));
 	objectPointsm.push_back(Point3f(2., 2., 0.));
-	//objectPointsm.push_back(Point3f(2., -2., 0.));
-	//objectPointsm.push_back(Point3f(3., 2., 0.));
 	objectPointsm.push_back(Point3f(3., 4., 0.));
 	objectPointsm.push_back(Point3f(3.,3., 0.));
 	objectPointsm.push_back(Point3f(3., 2., 0.));
-	//objectPointsm.push_back(Point3f(3., -2., 0.));
-	//objectPointsm.push_back(Point3f(3., -3., 0.));
-	//objectPointsm.push_back(Point3f(4., 3., 0.));
-	// objectPointsm.push_back(Point3f(4., 2., 0.));
-	// objectPointsm.push_back(Point3f(4., 1., 0.));
-	// objectPointsm.push_back(Point3f(4.,0., 0.));
-	// objectPointsm.push_back(Point3f(4., -1., 0.));
-	// objectPointsm.push_back(Point3f(4., -2., 0.));
-	// objectPointsm.push_back(Point3f(4., -3., 0.));
 
 	Mat rvec(1,3,DataType<double>::type);
 	Mat tvec(1,3,DataType<double>::type);
@@ -86,51 +49,24 @@ int main(int argc, char const *argv[])
 	Mat distCoeffs(4,1,DataType<double>::type);
 	Mat cameraMatrix(3,3,DataType<double>::type);	
 	Mat inlin;
+
 	setIdentity(cameraMatrix);
 	cameraMatrix.at<double>(0,0)=cameraMatrix.at<double>(1,1)=554.594;
 	cameraMatrix.at<double>(0,2)=320.5;
 	cameraMatrix.at<double>(1,2)=180.5;
+	
 	distCoeffs.at<double>(0) = 0;
 	distCoeffs.at<double>(1) = 0;
 	distCoeffs.at<double>(2) = 0;
 	distCoeffs.at<double>(3) = 0;
+	
 	cout<<cameraMatrix<<endl;
 
-	// int x=atoi(argv[2]);
-	// int min=31414,cont;
-	// for(int j=1;j<x;j++)
-	// {
-	// 	solvePnPRansac(objectPointsm, imagePointsPix, cameraMatrix, distCoeffs, rvec, tvec, 0, j,0.1,0.95	,inlin,CV_P3P);
-
-	// 	Rodrigues(rvec,rotationMatrix);
-	
-	// 	vector<Point2f> projectedPoints;
-
-	// 	projectPoints(objectPointsm, rvec, tvec, cameraMatrix, distCoeffs, projectedPoints);
-	// 	double sum=0;
-	// 	for(int i = 0; i < projectedPoints.size(); ++i)
-	// 	{
- //    		sum+=abs(projectedPoints[i].x-imagePointsPix[i].x)+abs(projectedPoints[i].y-imagePointsPix[i].y);
-	// 	}
-	// 	if(sum<min)
-	// 	{
-	// 		min=sum;
-	// 		cont=j;
-	// 	}
- //    //cout<<"\nSUM: "<<sum<<endl<<endl;
-	// 	cout<<"J: "<<j<<" SUM: "<<sum<<" MIN:"<<min<<endl;
-	// }
-	// cout<<"Count:"<<cont<< " "<<"MIN:"<<min;
-
-	// solvePnPRansac(objectPointsm, imagePointsPix, cameraMatrix, distCoeffs, rvec, tvec, 0, cont,0.1,0.95	,inlin,CV_P3P);
-	// Rodrigues(rvec,rotationMatrix);
-
 	setMouseCallback("Show", mouseCB, NULL);
-	//int y=atoi(argv[3]);
+
 	#if 1
 		int x=atoi(argv[2]);
 		int min=31414,cont;
-		cout<<"Hi"<<endl<<endl;
 		for(int j=1;j<x;j++)
 		{
 
@@ -172,7 +108,7 @@ int main(int argc, char const *argv[])
 			tempMat = rotationMatrix.inv() * cameraMatrix.inv() * uvPoint;
 			tempMat2 = rotationMatrix.inv() * tvec;
 
-			s = 0 + tempMat2.at<double>(2,0); //before 0 it was 285, which represents the height Zconst
+			s = 0 + tempMat2.at<double>(2,0); 
 			s /= tempMat.at<double>(2,0);
 			Mat output=s*uvPoint;
 
@@ -225,45 +161,3 @@ int main(int argc, char const *argv[])
 	}
 	#endif
 }
-/*
-XYZ=002
-Rvec:[1.764514762393209;
- -1.780685435244631;
- 0.7325915621812246]
-tvec:[-0.01909767291557224;
- 1.402113009725163;
- 1.462660406382692]
-
-Camera Cordinates [-0.01626623071641831;
- -0.001813630829758148;
- 2.026176990796318]
-
-
-
-XYZ=003
-Rvec:[1.745652658110562;
- -1.790957695767514;
- 0.7314882833614018]
-tvec:[-0.08459587385352374;
- 2.068372584492771;
- 2.025855824555773]
-
-Camera Cordinates [0.05319219390911223;
- -0.08930678410844205;
- 2.894582581454577]
-
-
-XYZ=032
-Rvec:[1.76901011450686;
- -1.765293277548755;
- 0.7166833678408217]
-tvec:[2.931487239060401;
- 1.351367248947931;
- 1.33945179956652]
-
-Camera Cordinates [0.02341496957262601;
- 2.928090952066225;
- 1.907793781647503]
-
-
-*/
